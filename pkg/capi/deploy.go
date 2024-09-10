d888b3297a6f26e5e0a2c360454511ae50e92a70
Andrey Smirnov <andrey.smirnov@siderolabs.com>
2024-09-10 17:34:45 +0400

    chore: update dependencies for Talos 1.8
    Use new Talos, new CAPI base, etc.
    
    Signed-off-by: Andrey Smirnov <andrey.smirnov@siderolabs.com>

--- pkg/capi/deploy.go
+++ pkg/capi/deploy.go
@@ -48,7 +48,7 @@ func DefaultDeployOptions() *DeployOptions {
 		ControlPlaneNodes: 1,
 		WorkerNodes:       1,
 		ClusterNamespace:  "default",
-		TalosVersion:      "v1.7",
+		TalosVersion:      "v1.8",
 		KubernetesVersion: constants.DefaultKubernetesVersion,
 	}
 }
