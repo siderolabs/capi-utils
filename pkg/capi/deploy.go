4ee8a1b7d4d006afa5c7f3b99032865c126b6d8a
Laura Brehm <laurabrehm@hey.com>
2025-11-24 17:07:22 +0100

    chore: update `DefaultDeployOptions` to Talos `v1.12`
    Prepare for Talos v1.12 release.
    
    Signed-off-by: Laura Brehm <laurabrehm@hey.com>

--- pkg/capi/deploy.go
+++ pkg/capi/deploy.go
@@ -48,7 +48,7 @@ func DefaultDeployOptions() *DeployOptions {
 		ControlPlaneNodes: 1,
 		WorkerNodes:       1,
 		ClusterNamespace:  "default",
-		TalosVersion:      "v1.11",
+		TalosVersion:      "v1.12",
 		KubernetesVersion: constants.DefaultKubernetesVersion,
 	}
 }
